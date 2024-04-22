fc1b3018bebfcefe24adf842119c70b8c1ff974e
Sreejith <sreejithsnair.sj@gmail.com>
2024-04-22 23:00:14 +0530

    Initial commit with string values added in xml

---
+++ app/src/main/java/com/sreejithsnair/sbank/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sreejithsnair.sbank.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sreejithsnair.sbank.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/sreejithsnair/sbank/viewmodel/BankRegistrationViewModel.kt
@@ -0,0 +1,4 @@
+package com.sreejithsnair.sbank.viewmodel
+
+class BankRegistrationViewModel {
+}
